74e66c0dd3d77f5b435700c381b0c1a7db3c11e2
SHAIK SAMEER BASHA <118707756+shaikSameerbasha5404@users.noreply.github.com>
2024-05-05 10:35:06 +0530

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,10 +1,32 @@
 using System;
-public class RecursiveFunction
-{
-    
-    }
-    public static void Main()
+namespace program11{
+    class Program
     {
-        
+        static int ReverseNumber(int num, int reversedNum = 0)
+        {
+           
+            if (num == 0)
+                return reversedNum;
+            
+           
+            int lastDigit = num % 10;
+            
+            
+            reversedNum = reversedNum * 10 + lastDigit;
+            
+            
+            return ReverseNumber(num / 10, reversedNum);
+        }
+    
+        static void Main(string[] args)
+        {
+         
+            Console.Write("Enter a number to reverse: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+    
+            
+            int reversedNumber = ReverseNumber(number);
+            Console.WriteLine("Reversed Number: " + reversedNumber);
+        }
     }
 }
